cd118b567303002d56ec66a8831d0924a76ba98e
Amy Haddad <amy.m.haddad@gmail.com>
2018-10-20 12:33:41 -0400

    example_problem.py

---
+++ main_example_problem.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 15:34:22 2018
+
+@author: amyhaddad
+"""
+
+from test_example_problem import tip_calculator
+
+def main():
+    """Calculate tip and total bill with tip"""
+
+# inputs for function
+    bill_amount = float(input("What is the bill? "))
+    tip_percent = float(input("What is the tip percent? "))
+
+# call function with variables
+    tip_calculation = tip_calculator(bill_amount, tip_percent)
+
+# coordinate output
+    bill_with_tip = bill_amount + tip_calculation
+
+# customize output
+    total_bill_statement = (f"The total with tip is ${bill_with_tip:.2f}.\n")
+    total_bill_statement += (f"The tip is ${tip_calculation:.2f}.")
+    print(total_bill_statement)
+
+main()
