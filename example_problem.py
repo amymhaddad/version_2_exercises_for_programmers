9bc796d069f13850375938459fd0d2d3a6a077f7
Amy Haddad <amy.m.haddad@gmail.com>
2018-10-29 06:48:13 -0400

    Update example_problem.py

--- example_problem.py
+++ example_problem.py
@@ -6,10 +6,15 @@ Created on Thu May 10 15:34:22 2018
 @author: amyhaddad
 """
 
-def tip_calculator(bill, tip_rate):
+def tip_calculator(bill_amount, tip_rate):
     """Determine the tip for a bill"""
 
     tip_as_decimal = tip_rate / 100
-    total_tip_amount = bill * tip_as_decimal
+    total_tip_amount = bill_amount * tip_as_decimal
 
     return total_tip_amount
+
+def calculate_total_bill(bill_amount, tip):
+    """"Calculate the total bill with tip"""
+
+    return bill_amount + tip
