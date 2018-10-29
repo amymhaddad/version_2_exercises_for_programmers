44f8eb76c09853989fc53b6ef79e4f3fea8543b7
Amy Haddad <amy.m.haddad@gmail.com>
2018-10-29 06:50:30 -0400

    Update test_example_problem.py

--- test_example_problem.py
+++ test_example_problem.py
@@ -7,7 +7,7 @@ Created on Thu May 10 15:34:22 2018
 """
 
 import unittest
-from example_problem import tip_calculator
+from example_problem import tip_calculator, calculate_total_bill
 
 class TipTestCase(unittest.TestCase):
     """Test for calculating tips at restaurants"""
@@ -19,6 +19,14 @@ class TipTestCase(unittest.TestCase):
         tip_rate = 20
         total_tip = tip_calculator(bill, tip_rate)
         self.assertEqual(total_tip, 20)
+
+    def test_bill_with_tip(self):
+        """"Test to calculate the total bill, with tip"""
+
+        bill = 100
+        tip = 20
+        total_bill = calculate_total_bill(bill, tip)
+        self.assertEqual(total_bill, 120)
 
 if __name__ == '__main__':
     unittest.main()
