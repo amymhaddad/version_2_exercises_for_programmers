db8b0dd80b69b70023a7f4ccbf3b3e7be71bdd1d
Amy Haddad <amy.m.haddad@gmail.com>
2018-10-29 07:55:36 -0400

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -6,7 +6,7 @@ Created on Thu May 10 15:34:22 2018
 @author: amyhaddad
 """
 
-def print_greeting(name):
+def generate_greeting(name):
     """Greet a user with a message"""
 
     greeting = (f"Hello, {name.title()}. It's nice to meet you!")
