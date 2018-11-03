29eecc07e2ba8c189f40f84a5ec508aa48776cad
Amy Haddad <amy.m.haddad@gmail.com>
2018-11-03 07:20:50 -0400

    Update main_exercise7.py

--- main_exercise7.py
+++ main_exercise7.py
@@ -9,7 +9,10 @@ Created on Thu May 10 15:34:22 2018
 from exercise7 import calculate_room_measurement_feet, convert_feet_to_meters
 
 def main():
-    """Get room dimensions and calcuate measurements in suare feet and square meters"""
+    """Get room dimensions and calcuate measurements in square feet and square meters"""
+
+    FEET_TO_METERS_CONVERSION = 0.09290304
+
     # get inputs and print a statement
     room_length = float(input("Enter the length of the room in feet: "))
     room_width = float(input("Enter the width of the room in feet: "))
@@ -18,7 +21,7 @@ def main():
 
     # call functions
     room_size_feet = calculate_room_measurement_feet(room_length, room_width)
-    room_size_square_meters = convert_feet_to_meters(room_size_feet)
+    room_size_square_meters = convert_feet_to_meters(room_size_feet, FEET_TO_METERS_CONVERSION)
 
     # coordinate and customize output
     print(f"The area is {int(room_size_feet)} square feet and {room_size_square_meters:.3f} square meters.")
