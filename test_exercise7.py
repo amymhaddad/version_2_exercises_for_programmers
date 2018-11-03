e738ebd9b3c84b97be94c81b9bed3a5f3644875c
Amy Haddad <amy.m.haddad@gmail.com>
2018-11-03 07:40:49 -0400

    Update test_exercise7.py

--- test_exercise7.py
+++ test_exercise7.py
@@ -18,16 +18,19 @@ class RoomCalculationsTest(unittest.TestCase):
 
         length = 15
         width = 20
+
         room_measure_feet = calculate_room_measurement_feet(length, width)
+
         self.assertEqual(room_measure_feet, 300.0)
 
     def test_feet_to_meters(self):
         """Test to convert feet to meters"""
 
         FEET_TO_METERS_CONVERSION = 0.09290304
-
         dimensions = 300
+
         room_from_feet_to_meters = convert_feet_to_meters(dimensions, FEET_TO_METERS_CONVERSION)
+
         self.assertEqual(room_from_feet_to_meters, 27.870912)
 
 if __name__ == '__main__':
