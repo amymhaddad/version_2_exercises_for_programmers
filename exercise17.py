7b01eb615fac57db105c905a30f4ab42a24f16aa
Amy Haddad <amy.m.haddad@gmail.com>
2018-11-21 10:41:45 -0500

    Update exercise17.py

--- exercise17.py
+++ exercise17.py
@@ -6,16 +6,23 @@ Created on Thu May 10 15:34:22 2018
 @author: amyhaddad
 """
 
+STANDARD_NUMBER_1 = 5.14
+MALE_ALCOHOL_DISTRIBUTION_RATIO = 0.73
+FEMALE_ALCOHOL_DISTRIBUTION_RATIO = 0.66
+STANDARD_NUMBER_2 = 0.15
+
 def calculate_male_bac(alcohol_in_ounces, body_weight, hours):
     """Create a calculate that calculates BAC for males"""
 
-    bac_male = (alcohol_in_ounces * 5.14 / body_weight * 0.73) - .015 * hours
+    bac_male = (alcohol_in_ounces * STANDARD_NUMBER_1 / body_weight\
+                * MALE_ALCOHOL_DISTRIBUTION_RATIO) - STANDARD_NUMBER_2 * hours
 
     return bac_male
 
 def calculate_female_bac(alcohol_in_ounces, body_weight, hours):
     """Create a calculate that calculates BAC for females"""
 
-    bac_female = (alcohol_in_ounces * 5.14 / body_weight * 0.66) - .015 * hours
+    bac_female = (alcohol_in_ounces * STANDARD_NUMBER_1 / body_weight\
+                 * FEMALE_ALCOHOL_DISTRIBUTION_RATIO) - STANDARD_NUMBER_2 * hours
 
     return bac_female
