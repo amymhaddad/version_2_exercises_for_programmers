e033aaf527a4dd837636ecbe18e10d1b796648d8
Amy Haddad <amy.m.haddad@gmail.com>
2018-11-21 10:40:53 -0500

    Update test_exercise17.py

--- test_exercise17.py
+++ test_exercise17.py
@@ -5,13 +5,12 @@ Created on Thu May 10 15:34:22 2018
 
 @author: amyhaddad
 """
-# recalcuate the assertEqual nubmer: enter the etnire number and make it a nubmer not a string
 
 import unittest
 from exercise17 import calculate_male_bac, calculate_female_bac
 
 class MaleBacTest(unittest.TestCase):
-    """Test for calculating male BAC"""
+    """Tests blood alcohol level for males and females"""
 
     def test_male_bac(self):
         """Calculate male bac"""
@@ -21,7 +20,7 @@ class MaleBacTest(unittest.TestCase):
         hours_since_last_drink = 1
 
         male_bac_calculation = calculate_male_bac(alcohol_amount, weight, hours_since_last_drink)
-        self.assertEqual(male_bac_calculation, 0.27363076923076923)
+        self.assertEqual(male_bac_calculation, 0.13863076923076925)
 
     def test_female_bac(self):
         """Calculate female bac"""
@@ -32,7 +31,7 @@ class MaleBacTest(unittest.TestCase):
 
         female_bac_calulcation = calculate_female_bac\
                                 (alcohol_amount, weight, hours_since_last_drink)
-        self.assertEqual(female_bac_calulcation, 0.24595384615384613)
+        self.assertEqual(female_bac_calulcation, 0.11095384615384615)
 
 if __name__ == '__main__':
     unittest.main()
