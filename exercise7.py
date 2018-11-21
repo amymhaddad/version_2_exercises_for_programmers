824008beb8b1b40e85ce623254ded319a89743b8
Amy Haddad <amy.m.haddad@gmail.com>
2018-11-21 10:54:41 -0500

    Update exercise7.py

--- exercise7.py
+++ exercise7.py
@@ -6,13 +6,15 @@ Created on Thu May 10 15:34:22 2018
 @author: amyhaddad
 """
 
+FEET_TO_METERS_CONVERSION = 0.09290304
+
 def calculate_room_measurement_feet(length, width):
     """Calculate the size of a room in feet"""
 
     area = length * width
     return area
 
-def convert_feet_to_meters(dimensions, FEET_TO_METERS_CONVERSION):
+def convert_feet_to_meters(dimensions):
     """Convert the dimensions from feet to meters"""
 
     square_meters = float(dimensions) * FEET_TO_METERS_CONVERSION
