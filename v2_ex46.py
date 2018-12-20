9b13c0c46adc90651a3d9ece236b74053173f7f2
Amy Haddad <amy.m.haddad@gmail.com>
2018-12-20 09:43:18 -0500

    Update v2_ex46.py

--- v2_ex46.py
+++ v2_ex46.py
@@ -7,24 +7,21 @@ Created on Thu May 10 15:34:22 2018
 """
 
 import csv
-new_words = [words for words in csv.reader(open("text_file.txt", newline=''))]
 
-line_one_split_list_of_words = [word.split() for word in new_words[0]]
-line_two_split_list_of_words = [word.split() for word in new_words[1]]
-total_list = line_one_split_list_of_words + line_two_split_list_of_words
+filename = 'text_file.txt'
 
 string_of_words = ''
-for words in total_list:
-    for word in words:
-        string_of_words += word + ' '
 
-set_unique_words = set(string_of_words.split())
-list_of_unique_words = [word for word in set_unique_words]
+with open(filename, newline='') as csvfile:
+    linereader = csv.reader(csvfile, delimiter=' ')
+    for rows in linereader:
+        for row in rows:
+            string_of_words += row + ' '
+            list_of_words = string_of_words.split()
 
-final_output = ''
+unique_words = set(list_of_words)
 
-for word in list_of_unique_words:
-    if word in string_of_words:
-        word_count = string_of_words.count(word)
-        final_output += f"{word}: {word_count}\n"
-print(final_output.strip())
+for word in unique_words:
+    frequency = string_of_words.count(word)
+    final_output = f"{word}: {frequency}\n"
+    print(final_output.strip())
