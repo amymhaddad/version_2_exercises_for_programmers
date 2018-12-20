1e9ba762813b78a1c55f1f36febb8c10e9721fcb
Amy Haddad <amy.m.haddad@gmail.com>
2018-12-20 11:12:43 -0500

    Update csv_exercise40.py

--- csv_exercise40.py
+++ csv_exercise40.py
@@ -1,36 +1,28 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 10 15:34:22 2018
-
-@author: amyhaddad
-"""
-
 import csv
 
-imported_data = [fields for fields in csv.reader(open("data_exercise40.txt", newline =''))]
+FILENAME = 'data_exercise40.txt'
 
 user_search = input("Enter your search: ")
 normalized_user_search = user_search.title()
 
-total_employee_data = [details for details in imported_data]
+with open(FILENAME, newline='') as csvfile:
+        linereader = csv.reader(csvfile, delimiter=',')
+        list_of_employee_data = []
+        for employee in linereader:
+                list_of_employee_data.append(employee)
 
-header_row = total_employee_data[0]
-del header_row[0:2]
-header_row.insert(0, "Fullname")
-string_of_headers = ' | '.join(header_row) 
-row_of_dashes = "-"*len(string_of_headers)
+headers = "   ".join(list_of_employee_data[0])
+row_of_dashes = '-' * len(headers)
 
 employee_data = ''
 
-for employees in total_employee_data[1:]:
-        full_name_format = ' '.join(employees[:2]) 
-        position_format = employees[2]
-        separation_date_format = employees[3]
+for employee in list_of_employee_data[1:]:
+        full_name = employee[0] + ' ' + employee[1]
+        position = employee[2]
+        separation_date = employee[3]
 
-        if normalized_user_search in full_name_format:
-                employee_data += full_name_format + ' | ' + position_format + ' | ' + separation_date_format + '\n'
+        if normalized_user_search in full_name:
+                employee_data += full_name + '  |  ' + position + '  |  ' + separation_date
 
-final_format = f"{string_of_headers}\n{row_of_dashes}\n{employee_data}"
-
+final_format = f"{headers}\n{row_of_dashes}\n{employee_data}"
 print(final_format)
