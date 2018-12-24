1d18cb73aea5a230465a2300a41c14bf0a3feb4e
Amy Haddad <amy.m.haddad@gmail.com>
2018-12-24 12:00:31 -0500

    Update flower_reader.py

--- flower_reader.py
+++ flower_reader.py
@@ -1,33 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 Created on Thu May 10 15:34:22 2018
 
 @author: amyhaddad
 """
 
 FILENAME = 'flowers.txt'
-FLOWERS = {}
 user_name = input('Enter your first and last name: ')
 find_first_letters_of_user_name = lambda user_name: user_name[0].title()
 
-def create_flower_dictionary(FILENAME):
+def create_flower_dictionary(file_name):
     """Read in a file and create a dictionary"""
 
-    with open(FILENAME) as f_object:
+    flowers = {}
+
+    with open(file_name) as f_object:
         contents = f_object.readlines()
         for flower in contents:
             split_flower_content = flower.split(" ", 1)
             letter = split_flower_content[0]
             flower_name = split_flower_content[1]
-            FLOWERS[letter] = flower_name
+            flowers[letter] = flower_name
 
-        return FLOWERS
+        return flowers
 
-def match_letter_to_flower(user_name, FILENAME):
+def match_letter_to_flower(user_name, file_name):
     """Match the first letter of user's name to the flower type"""
 
-    flower_dictionary = create_flower_dictionary(FILENAME)
+    flower_dictionary = create_flower_dictionary(file_name)
     first_letter = find_first_letters_of_user_name(user_name)
 
     for letters, flower_names in flower_dictionary.items():
@@ -36,3 +40,4 @@ def match_letter_to_flower(user_name, FILENAME):
 
 matched_name_to_flower = match_letter_to_flower(user_name, FILENAME)
 print(matched_name_to_flower.strip())
+
