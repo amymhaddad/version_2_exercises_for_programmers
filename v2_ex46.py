8d59866fcb6900c5efecec72301b2653cd2ca832
Amy Haddad <amy.m.haddad@gmail.com>
2018-12-24 11:21:20 -0500

    Update v2_ex46.py

--- v2_ex46.py
+++ v2_ex46.py
@@ -31,3 +31,32 @@ with open(filename, newline ='') as csvfile:
     sorted_dictionary = sorted(frequency_tracker.items(), key=lambda t: t[1], reverse=True)
     for item in sorted_dictionary:
         print(f"{item[0]}: {item[1]}")
+
+        
+        
+        
+        
+#Iteration 2:
+
+import csv
+from collections import Counter
+
+filename = 'text_file.csv'
+
+with open(filename, newline='') as csvfile:
+    linereader = csv.reader(csvfile, delimiter=',')
+    string_of_words = ''
+    for rows in linereader:
+        for row in rows:
+            string_of_words += row + ' '
+            list_of_words = string_of_words.split()
+
+    counters = Counter(list_of_words).most_common()
+
+    frequently_used_words_with_counter = [f"{word}: {count}" for word, count in counters]
+
+    formatted_word_frequency = ''
+    for word in frequently_used_words_with_counter:
+        formatted_word_frequency += word + '\n'
+
+    print(formatted_word_frequency)
