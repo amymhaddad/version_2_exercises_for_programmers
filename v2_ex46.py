d5f05ae215128d72ebe0772741b92bfe6d6e9b2d
Amy Haddad <amy.m.haddad@gmail.com>
2018-12-26 11:56:05 -0500

    Update v2_ex46.py

--- v2_ex46.py
+++ v2_ex46.py
@@ -23,16 +23,14 @@ with open(filename, newline ='') as csvfile:
 
     frequency_tracker = {}
     for word in unique_words:
-        if word in list_of_words:
-            frequency = list_of_words.count(word)
-            frequency_tracker[word] = frequency
+        frequency = list_of_words.count(word)
+        frequency_tracker[word] = frequency
 
     frequencies = OrderedDict(sorted(frequency_tracker.items(), key=lambda t: t[1], reverse=True))
 
     for word, count in frequencies.items():
         astericks = '*' * count
         print(f"{word}: {astericks}")
-
 
 #########
 #Iteration 2:
@@ -51,8 +49,6 @@ with open(filename, newline='') as csvfile:
             list_of_words = string_of_words.split()
 
     counters = Counter(list_of_words).most_common()
-
-    frequently_used_words_with_counter = [f"{word}: {count}" for word, count in counters]
 
     for word, number in counters:
         astericks = '*' * number
